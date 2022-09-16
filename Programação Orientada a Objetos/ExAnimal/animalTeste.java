e4e3690e1bc1046d61fa9712841b72207b560bad
Juliagali <juliagalibarroso@gmail.com>
2022-09-15 23:49:28 -0300

    exercício 2

--- Programação Orientada a Objetos/ExAnimal/animalTeste.java
+++ Programação Orientada a Objetos/ExAnimal/animalTeste.java
@@ -1,27 +1,19 @@
 package POO;
 
-public class animalTeste {
+public class testeAnimal {
 
-	public static void main(String[] args) {
-		
-	cachorro dog = new cachorro("Cachorro", 7, "Domesticação na Europa ou na Ásia", "Sim", "Anda e corre", "Salsicha");
-	cavalo horse = new cavalo ("Cavalo", 10, "Acredita-se em América do Norte", "Sim", "Cavalga e corre", "Baixadeiro");	
-	preguiça sloth = new preguiça("Bicho Preguiça", 5, " Mata Atlântica e da Amazônia", "Sim", "Sobe árvores");
-	
-System.out.println("\t\t\tInformações dos animais: ");
-dog.imprimirCa();
-horse.imprimirCav();
-sloth.imprimirPre();
+    public static void main(String[] args) {
 
-	
-	
-		
-		
-		
-		
-		
-		
-		
-	}	
-	
-}
+
+            cachorro cachorro = new cachorro("Doguinho" , 7);
+            cavalo cavalo = new cavalo ("Faísca" , 12);
+            preguiça preguiça = new preguiça("Sleep" , 3);
+
+            System.out.println("\n\t\tInformações dos animais: ");
+
+            cachorro.imprimirC();
+            horse.imprimirCav();
+            sloth.imprimirPre();
+
+            }
+    }
