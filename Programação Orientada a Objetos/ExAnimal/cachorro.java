7d3cb14b0f6d22a43b0a5e0f93cce316f8cf7acd
Juliagali <juliagalibarroso@gmail.com>
2022-09-15 23:47:46 -0300

    sub-classe

--- Programação Orientada a Objetos/ExAnimal/cachorro.java
+++ Programação Orientada a Objetos/ExAnimal/cachorro.java
@@ -1,53 +1,28 @@
 package POO;
 
-public class cachorro extends animal {
-	
-	//atributos
-	private String deslocamento;
-	private String raca;	
-	
-	//construtor
-	public cachorro(String nome, int idade, String origem, String som, String deslocamento, String raca) {
-		super(nome, idade, origem, som);
-		this.deslocamento = deslocamento;
-		this.raca = raca;
-	} //fim do construtor
+public class cachorro extends animal{ //criando herança
 
-	
-	
-	//métodos
-	public String getDeslocamento() {
-		return deslocamento;
-	}
+    public cachorro(String nome, int idade) {
+        super(nome, idade);
+        this.nome = nome;
+        this.idade = idade;
+    }
 
-	public void setDeslocamento(String deslocamento) {
-		this.deslocamento = deslocamento;
-	}
-	
-	public String getRaca() {
-		return raca;
-	}
+    //métodos
+    @Override
+    public void emitirSom(String emitirSom) {
+        System.out.println("\nO cachorro late");
+    }
 
+    @Override
+    public void deslocamento(String deslocamento) {
+        System.out.println("\nO cachorro corre");
+    }
 
+    public void imprimirC(String emitirSom, String deslocamento) {
+        System.out.println("\nNome: " + getNome() + "\nIdade: " + getIdade() + "\nSom emitido: " + emitirSom + 
+        "\nDeslocamento: " + deslocamento);
+    }
 
-	public void setRaca(String raca) {
-		this.raca = raca;
-	}
-
-
-
-	public void imprimirCa(){
-		System.out.println("\nNome do animal: " + getNome() + "\nIdade: " + getIdade() + "\nOrigem do animal: " + getOrigem() +
-		"\nEmite som?: " + getSom() + "\nDeslocamento: " + deslocamento + "\nRaça: " + raca);
-	}
-	
-	
-	 
-	
-	
-	
-	
-	
-	
 
 }
