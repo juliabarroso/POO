e45410eea52a3a3c40314d39515ff3ce5b1618a6
Juliagali <juliagalibarroso@gmail.com>
2022-09-15 23:48:35 -0300

    sub-classe

--- Programação Orientada a Objetos/ExAnimal/cavalo.java
+++ Programação Orientada a Objetos/ExAnimal/cavalo.java
@@ -2,47 +2,29 @@ package POO;
 
 public class cavalo extends animal {
 	
-		//atributos
-		private String deslocamento;
-		private String raca;
+	public cavalo(String nome, int idade) {
+        super(nome, idade);
+        this.nome = nome;
+        this.idade = idade;
+    }
 
-		//construtor
-		public cavalo(String nome, int idade, String origem, String som, String deslocamento, String raca) {
-			super(nome, idade, origem, som);
-			this.deslocamento = deslocamento;
-			this.raca = raca;
-		} //fim do construtor
+    @Override
+    public void emitirSom(String emitirSom) {
+        System.out.println("\nO cavalo relincha");
+    }
 
-		
-		
-		//métodos
-		public String getDeslocamento() {
-			return deslocamento;
-		}
+    @Override
+    public void deslocamento(String deslocamento) {
+        System.out.println("O cavalo corre");
+    }
 
-		public void setDeslocamento(String deslocamento) {
-			this.deslocamento = deslocamento;
-		}
-		
-		public String getRaca() {
-			return raca;
-		}
+    public void imprimirCv(String emitirSom, String deslocamento) {
+        System.out.println("\nNome: " + getNome() + "\nIdade: " + getIdade() + "\nSom emitido: " + emitirSom + 
+        "\nDeslocamento: " + deslocamento);
+    }
 
-
-
-		public void setRaca(String raca) {
-			this.raca = raca;
-		}
-
-
-
-		public void imprimirCav(){
-			System.out.println("\nNome do animal: " + getNome() + "\nIdade: " + getIdade() + "\nOrigem do animal: " + getOrigem() +
-			"\nEmite som?: " + getSom() + "\nDeslocamento: " + deslocamento + "\nRaça: " + raca);
-		}
-		
 	
-	
+		
 	
 	
 
